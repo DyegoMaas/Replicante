2de44c5100522f93d34889323892da16400eeffa
Dyego Maas <dyego.maas@gmail.com>
2020-08-09 09:53:11 -0300

    Extracted file name replacements into a samply.yaml file.

--- src/replicator.py
+++ src/replicator.py
@@ -5,30 +5,7 @@ import os, shutil, codecs
 import argparse
 import yaml
 
-
-class ReplicationRecipe:
-    def __init__(self, template_name, template_dir):
-        super().__init__()
-        self.template_name = template_name
-        self.template_dir = template_dir
-
-    @staticmethod
-    def from_yaml_dict(yaml_dict):
-        template_name = yaml_dict['templateName']
-        return ReplicationRecipe(
-            template_name=template_name,
-            template_dir=f'./_templates/{template_name}',
-        )
-
-def __replace_terms_in_text(text: str, replacements) -> str:
-    for replacement in replacements:
-        old, new = replacement
-        text = text.replace(old, new)
-
-    return text
-
-
-def __remove_bom(full_path):
+def remove_bom(full_path):
     BUFSIZE = 4096
     BOMLEN = len(codecs.BOM_UTF8)
 
@@ -47,101 +24,132 @@ def __remove_bom(full_path):
             fp.truncate()
 
 
-def __prepare_file(file_path, lines_to_prepend, variables, source_code_replacements):
-    with open(file_path, 'r', encoding='utf-8') as original:
-        original_content = original.read()
-        with open(file_path, 'w', encoding='utf-8') as modified:
-            for line in lines_to_prepend:
-                line = __replace_terms_in_text(line, source_code_replacements)
-                modified.write(f'{line}\n')
+class ReplicationRecipe:
+    def __init__(self, template_name, template_dir):
+        super().__init__()
+        self.template_name = template_name
+        self.template_dir = template_dir
 
-            adjusted_content = variables + __replace_terms_in_text(original_content, source_code_replacements)
-            modified.write(adjusted_content.lstrip())
+    @staticmethod
+    def from_yaml_dict(yaml_dict):
+        template_name = yaml_dict['templateName']
+        return ReplicationRecipe(
+            template_name=template_name,
+            template_dir=f'./src/_templates/{template_name}', # TODO review
+        )
 
 
-def to_template(src, dest, relative_path):
-    full_src = os.path.abspath(src)
-    full_dest = f'{os.path.abspath(dest)}.ejs.t'
+class Replicator:
+    def __init__(self, replication_recipe: ReplicationRecipe):
+        super().__init__()
+        self.replication_recipe = replication_recipe
 
-    print(f'Generating template from {full_src} to {full_dest}')
-    copyfile(full_src, full_dest) # merge two parts
-    __remove_bom(full_dest)
 
-    target_path = relative_path.replace('\\', '/')
-    target_path = __replace_terms_in_text(target_path, replacements=[
-        ('PrecosClientes', '<%= name %>'), # TODO parameterize
-        ('Precos.Clientes', '<%= name %>'),
-    ])
-    frontmatter = [
-        '---',
-        f'to: <%= name %>/{target_path}',
-        'force: true',
-        '---',
-    ]
-    variables = (
-        '<%'
-        + 'NameUpperCase = name.toUpperCase();'
-        + 'NameLowerCase = name.toLowerCase();'
-        + 'NameLowerDasherized = h.inflection.dasherize(NameLowerCase);'
-        + 'NameCapitalized = h.inflection.capitalize(name);'
-        + 'ChartName = NameLowerCase; %>'
-        # TODO pegar outras via prompt do Hygen
-    )
-    __prepare_file(full_dest,
-        lines_to_prepend=frontmatter,
-        variables=variables,
-        source_code_replacements=[ # TODO parameterize
-            ('precosclientes', '<%= ChartName %>'),
-            ('PrecosClientes', '<%= name %>'),
+    def __replace_terms_in_text(self, text: str, replacements) -> str:
+        for replacement in replacements:
+            old, new = replacement
+            text = text.replace(old, new)
+
+        return text
+
+
+    def __prepare_file(self, file_path, lines_to_prepend, variables, source_code_replacements):
+        with open(file_path, 'r', encoding='utf-8') as original:
+            original_content = original.read()
+            with open(file_path, 'w', encoding='utf-8') as modified:
+                for line in lines_to_prepend:
+                    line = self.__replace_terms_in_text(line, source_code_replacements)
+                    modified.write(f'{line}\n')
+
+                adjusted_content = variables + self.__replace_terms_in_text(original_content, source_code_replacements)
+                modified.write(adjusted_content.lstrip())
+
+
+    def to_template(self, src, dest, relative_path):
+        full_src = os.path.abspath(src)
+        full_dest = f'{os.path.abspath(dest)}.ejs.t'
+
+        print(f'Generating template from {full_src} to {full_dest}')
+        copyfile(full_src, full_dest) # merge two parts
+        remove_bom(full_dest)
+
+        target_path = relative_path.replace('\\', '/')
+        target_path = self.__replace_terms_in_text(target_path, replacements=[
+            ('PrecosClientes', '<%= name %>'), # TODO parameterize
             ('Precos.Clientes', '<%= name %>'),
-            ('PRECOS-CLIENTES', 'PRECOS-<%= NameUpperCase %>'),
-            ('Precos-Clientes', 'Precos-<%= name %>'),
-            ('precos-clientes', 'precos-<%= NameLowerCase %>'),
-            ('precos_clientes', 'precos_<%= NameLowerCase %>'),
-            ('clientes', '<%= NameLowerCase %>'),
-            ('Clientes', 'Preços - <%= Name %>'),
+        ])
+        frontmatter = [
+            '---',
+            f'to: <%= name %>/{target_path}',
+            'force: true',
+            '---',
         ]
-    )
+        variables = (
+            '<%'
+            + 'NameUpperCase = name.toUpperCase();'
+            + 'NameLowerCase = name.toLowerCase();'
+            + 'NameLowerDasherized = h.inflection.dasherize(NameLowerCase);'
+            + 'NameCapitalized = h.inflection.capitalize(name);'
+            + 'ChartName = NameLowerCase; %>'
+            # TODO pegar outras via prompt do Hygen
+        )
+        self.__prepare_file(full_dest,
+            lines_to_prepend=frontmatter,
+            variables=variables,
+            source_code_replacements=[ # TODO parameterize
+                ('precosclientes', '<%= ChartName %>'),
+                ('PrecosClientes', '<%= name %>'),
+                ('Precos.Clientes', '<%= name %>'),
+                ('PRECOS-CLIENTES', 'PRECOS-<%= NameUpperCase %>'),
+                ('Precos-Clientes', 'Precos-<%= name %>'),
+                ('precos-clientes', 'precos-<%= NameLowerCase %>'),
+                ('precos_clientes', 'precos_<%= NameLowerCase %>'),
+                ('clientes', '<%= NameLowerCase %>'),
+                ('Clientes', 'Preços - <%= Name %>'),
+            ]
+        )
 
-def __process_files_in_directory(path, root_path, template_dir, replication_recipe: ReplicationRecipe):
-    print(f'Processing directory {path}')
+    def __process_files_in_directory(self, path, root_path):
+        print(f'Processing directory {path}')
 
-    for file in os.listdir(path):
-        full_path = os.path.join(path, file)
+        for file in os.listdir(path):
+            full_path = os.path.join(path, file)
 
-        if os.path.isfile(full_path):
-            relative_path = os.path.relpath(full_path, root_path)
-            middle_path = relative_path.replace(os.path.basename(relative_path), '')
-            virtual_path = os.path.join(middle_path, file).replace('\\', '-').replace('/', '-')
-            to_template(full_path, f'{template_dir}/new/{virtual_path}', relative_path)
-            continue
+            if os.path.isfile(full_path):
+                relative_path = os.path.relpath(full_path, root_path)
+                middle_path = relative_path.replace(os.path.basename(relative_path), '')
+                virtual_path = os.path.join(middle_path, file).replace('\\', '-').replace('/', '-')
+                self.to_template(full_path, f'{self.replication_recipe.template_dir}/new/{virtual_path}', relative_path)
+                continue
 
-        directory = file
-        if directory in ['.git', '.idea', 'Replicant', 'docs', 'bin', 'obj']: # TODO parameterize
-            continue
+            directory = file
+            if directory in ['.git', '.idea', 'Replicant', 'docs', 'bin', 'obj']: # TODO parameterize
+                continue
 
-        __process_files_in_directory(full_path, root_path, template_dir, replication_recipe)
-
-
-def __clean_template_directory(template_dir):
-    if not os.path.exists(template_dir):
-        os.mkdir(template_dir)
-
-    new_command_dir = os.path.join(template_dir, 'new')
-    if not os.path.exists(new_command_dir):
-        os.mkdir(new_command_dir)
-
-    for file in os.listdir(new_command_dir):
-        file_name = os.path.abspath(os.path.join(new_command_dir, file))
-
-        if os.path.isfile(file_name):
-            os.remove(file_name)
-        else:
-            rmtree(file_name, ignore_errors=False)
+            self.__process_files_in_directory(full_path, root_path)
 
 
-def __process_files_in_path(path, template_dir, replication_recipe: ReplicationRecipe):
-    __process_files_in_directory(path, path, template_dir, replication_recipe)
+    def clean_template_directory(self):
+        template_dir = self.replication_recipe.template_dir
+
+        if not os.path.exists(template_dir):
+            os.mkdir(template_dir)
+
+        new_command_dir = os.path.join(template_dir, 'new')
+        if not os.path.exists(new_command_dir):
+            os.mkdir(new_command_dir)
+
+        for file in os.listdir(new_command_dir):
+            file_name = os.path.abspath(os.path.join(new_command_dir, file))
+
+            if os.path.isfile(file_name):
+                os.remove(file_name)
+            else:
+                rmtree(file_name, ignore_errors=False)
+
+
+    def process_recipe_files(self, path):
+        self.__process_files_in_directory(path, root_path=path)
 
 
 def __load_replication_config(replication_config_file_path) -> ReplicationRecipe:
@@ -153,13 +161,13 @@ def __load_replication_config(replication_config_file_path) -> ReplicationRecipe
 
 def update_template(replication_instructions):
     sample_dir = replication_instructions['sample_directory'] # TODO include sample_dir in config loading somehow
-    replication_recipe = __load_replication_config(replication_instructions['replication_recipe_file'])
+    recipe = __load_replication_config(replication_instructions['replication_recipe_file'])
 
-    __clean_template_directory(replication_recipe.template_dir)
-    __process_files_in_path(sample_dir, replication_recipe.template_dir, replication_recipe)
+    replicator = Replicator(replication_recipe=recipe)
+    replicator.clean_template_directory()
+    replicator.process_recipe_files(sample_dir)
 
 
-# TODO receive project dir and config file
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sample', dest='sample_directory', required=True)
