583ffb17935da8b3bcd729e3859cbe3978da0ebe
Dyego Alekssander Maas <dyego.maas@gmail.com>
2020-08-11 23:08:31 -0300

    First tests about the virtual tree in the generated template folder.

--- test/common/replication.js
+++ test/common/replication.js
@@ -1,4 +1,5 @@
-const { spawn, exec } = require("child_process"); // TODO refactor to use spawn instead (less memory intensive)
+const { spawn, exec } = require('child_process'); // TODO refactor to use spawn instead (less memory intensive)
+const fs = require('fs');
 
 /**
  * Executes a shell command and return it as a Promise.
@@ -6,7 +7,6 @@ const { spawn, exec } = require("child_process"); // TODO refactor to use spawn 
  * @return {Promise<string>}
  */
 function execShellCommand(cmd) {
-    const exec = require('child_process').exec;
     return new Promise((resolve, reject) => {
         exec(cmd, (error, stdout, stderr) => {
             if (error) {
@@ -19,25 +19,24 @@ function execShellCommand(cmd) {
    
 const replicate = async function(sample, recipe) {
     return execShellCommand(`python ./src/replicate.py --sample=${sample} --recipe=${recipe}`)
-    // var x = 'lalala';
-    // exec(`python ./src/replicate.py --sample=${sample} --recipe=${recipe}`, (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.log(`error: ${error.message}`);
-    //         done();
-    //         return;
-    //     }
-    //     if (stderr) {
-    //         console.log(`stderr: ${stderr}`);
-    //         done();
-    //         return;
-    //     }
-    //     console.log(`stdout: ${stdout}`);
-    //     x = x + 'lu';
-    //     done();
-    // });
-    // return x;
+}
+
+const loadRecipe = function(recipe) {
+    let rawData = fs.readFileSync(recipe);
+    return JSON.parse(rawData);
+}
+
+const readTemplateForRecipe = function(recipe) {
+    return fs.readdirSync(`./src/_templates/${recipe.templateName}/new`);
+}
+
+const deleteTemplateForRecipe = function(recipe) {
+    return fs.rmdirSync(`./src/_templates/${recipe.templateName}`, { recursive: true });
 }
 
 module.exports = {
-    replicate: replicate
+    replicate: replicate,
+    loadRecipe: loadRecipe,
+    readTemplateForRecipe: readTemplateForRecipe,
+    deleteTemplateForRecipe: deleteTemplateForRecipe
 }
--- test/test-file-replacements.js
+++ test/test-file-replacements.js
@@ -1,14 +1,31 @@
 var expect  = require('chai').expect;
-const { spawn, exec } = require('child_process'); // TODO refactor to use spawn instead (less memory intensive)
-const { replicate } = require("./common/replication")
+const { replicate, loadRecipe, readTemplateForRecipe, deleteTemplateForRecipe } = require("./common/replication");
 
 describe('File name and directory tree replacements', function () {
+    const recipeFilePath = './test/fixtures/helloworld-to-hithere-recipe.json';
+    const recipe = loadRecipe(recipeFilePath);
+    const templateFiles = [];
+
     before(async function () {
-        const output = await replicate('./test/fixtures/hello-world', './test/fixtures/helloworld-to-hithere-recipe.json');
+        const output = await replicate('./test/fixtures/hello-world', recipeFilePath);
         console.log(`Replication output: ${output}`);
+
+        readTemplateForRecipe(recipe).map(file => templateFiles.push(file));
     });
 
-    it('Should replace file name sctructure', function() {
-        expect('Hello World').to.equal('Hello World');
+    after(function () {
+        deleteTemplateForRecipe(recipe);
+    });
+
+    it('Should include all files in the source file tree', function() {
+        expect(templateFiles.length).to.equal(3);        
+    });
+
+    it('Should use virtual path structure separated by hyphen', function() {
+        // Hello
+        // --There
+        // ----World.js
+        // turns into Hello-There-World.js.ejs.t
+        expect(templateFiles).to.contain('Hello-There-World.js.ejs.t');        
     });
 });
