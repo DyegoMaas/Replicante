27f85d358998725a490fe19dbdaf305cd618fb22
Dyego Maas <dyego.maas@gmail.com>
2020-08-17 20:44:34 -0300

    Bumping version to 0.5.1.

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -22,13 +22,13 @@ const {generateReplicant, resolveReplicantWorkDir} = require('../src/replication
 describe('Versioning', () => {
     test('It should output version', async () => {
         const output = await cli('--version')
-        expect(output).toContain('0.5.0')
+        expect(output).toContain('0.5.1')
     });
 
     test('It should output help', async () => {
         // TODO improve this test
         const output = await cli('--help')
-        expect(output).toContain('0.5.0')
+        expect(output).toContain('0.5.1')
     });
 });
 
