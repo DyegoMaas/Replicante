17527957130d4af7408f5788cdea29aba0016434
Dyego Maas <dyego.maas@gmail.com>
2020-08-18 14:17:12 -0300

    Bumping version to 0.6.0.

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -27,13 +27,13 @@ describe('CLI tests', () => {
     describe('Versioning', () => {
         test('It should output version', async () => {
             const output = await cli('--version')
-            expect(output).toContain('0.5.2')
+            expect(output).toContain('0.6.0')
         });
 
         test('It should output help', async () => {
             // TODO improve this test
             const output = await cli('--help')
-            expect(output).toContain('0.5.2')
+            expect(output).toContain('0.6.0')
         });
     });
 
