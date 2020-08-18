7b9d7101e1f7f92e50e94f96759efd1342621066
Dyego Maas <dyego.maas@gmail.com>
2020-08-17 22:48:08 -0300

    Adding a default command.

--- src/cli.ts
+++ src/cli.ts
@@ -11,6 +11,7 @@ async function run(argv) {
     .plugins('./node_modules', { matching: 'replicante-*', hidden: true })
     .help() // provides default for help, h, --help, -h
     .version() // provides default for version, v, --version, -v
+    .defaultCommand()
     .create()
   // enable the following method if you'd like to skip loading one of these core extensions
   // this can improve performance if they're not necessary for your project:
