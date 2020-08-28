d0d9681e336f3a3d7c2a3cf7c1a08668a9a45452
Dyego Alekssander Maas <dyego.maas@gmail.com>
2020-08-27 22:45:45 -0300

    Bumping version to 0.9.

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -28,7 +28,7 @@ describe('CLI tests', () => {
   })
 
   describe('Versioning', () => {
-    const expectedVersion = '0.8.0'
+    const expectedVersion = '0.9.0'
 
     test('It should output version', async () => {
       const output = await cli('--version')
